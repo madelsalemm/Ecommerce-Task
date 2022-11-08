cbfdae11d6df21486a7699d85ef1921a4ffc253a
madelsalemm <Mohamed.adel.salemm@gmail.com>
2022-11-08 20:25:12 +0200

    Creat app (account) with all templates and urls file and settings

---
+++ account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path , include
+
+
+app_name = 'account'
+
+urlpatterns = [
+    #path('', Home.as_view(), name='home'),
+    
+]
--- project/urls.py
+++ project/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path , include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('' , include('product.urls' , namespace= 'product')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('account.urls' , namespace= 'accounts' )),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
