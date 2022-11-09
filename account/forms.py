736a0c170ba8b23f7cac43212dbb8b74c91229dd
madelsalemm <Mohamed.adel.salemm@gmail.com>
2022-11-09 02:17:53 +0200

    add signup & product list& detail (views , urls ,templates)and base template REV02

--- account/forms.py
+++ account/forms.py
@@ -1,16 +1,7 @@
-from dataclasses import fields
-from pyexpat import model
 from django import forms
-from . import models
 from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username' , 'first_name' , 'last_name' , 'email']
-
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = models.Profile
-        fields = ['image' , 'country' , 'PRFaddress' ]
